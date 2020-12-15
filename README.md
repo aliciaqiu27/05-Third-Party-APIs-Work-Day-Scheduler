cd9f4a9dd1e6b8a00dc331216edfbba5f2472b1c
Alicia Qiu Cheung <aliciaqiu27@gmail.com>
2020-12-15 00:00:31 -0700

    05-third-party-apis-homework-demo.gif

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 05-Third-Party-APIs-Work-Day-Scheduler
+
+
+
+The library [Luxon](https://moment.github.io/luxon/) was used to grab the date and times from the third-party API. The purpose of this project is to help a busy client to create a daily work planner that 
+## User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
--- script.js
+++ script.js
@@ -1,45 +1,61 @@
+   // $(".saveBtn").on("click", function(event) {
 
-    $(".saveBtn").on("click", function(event) {
+   //    // Preventing the submit button from trying to submit the form
+   //    event.preventDefault();
+   //    let whatToDo = $(this).siblings("textarea").val();
+   //       console.log(whatToDo);
+   //    let whatToDoHour = $(this).attr("id");
+   //       console.log(whatToDoHour);
 
-    // Preventing the submit button from trying to submit the form
-    event.preventDefault();
-    let textArea = $("textarea")
-    
-    // create user object from submission
-    var user = {
-    firstName: $('textArea').val(),
-    };
+   //    var user = localStorage.setItem(whatToDoHour,whatToDo);
+   //    console.log(user);
 
-    console.log(user);
-  
-    // set new submission
-    localStorage.setItem("user", JSON.stringify(user));
+   //    // get most recent submission
+   //    $("#14").text(window.localStorage.getItem("14"));
+   //    $("#15").text(window.localStorage.getItem("15"));
 
-    // get most recent submission
-    var lastUser = JSON.parse(localStorage.getItem("user"));
-    $('textarea').append(user)
-    });
-    
+   //    // let myStorage = window.localStorage
+   //    // console.log(savedSchedule);
+   // });
+   $('.saveBtn').on('click', saveToLocalStorage);
 
-
-    //Set current time top at the top of the page
-    var currentDate = luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' });;
-    console.log(currentDate);
-    $("#currentDay").text(currentDate);
-    var currentTimeId = luxon.DateTime.local().get('hour');
-
-
-    // time-block.each()
-        $(".time-block").each(function(){
-            var timeBlockHour = this.id
-            console.log(timeBlockHour);
-        if (timeBlockHour < currentTimeId){
-           $(this).find('textarea').addClass('past');
-        }
-        else if (timeBlockHour > currentTimeId){
-           $(this).find('textarea').addClass('future');
-        }
-        else {
-           $(this).find('textarea').addClass('present');
-        }
+      function saveToLocalStorage() {
+        $('textarea').each(function () {
+            var whatToDo = $(this).val();
+            var whatToDoHour = $(this).attr("id");
+            localStorage.setItem(whatToDoHour, whatToDo);
         });
+      };
+    
+      $('#9').text(window.localStorage.getItem('9'));
+      $('#10').text(window.localStorage.getItem('10'));
+      $('#11').text(window.localStorage.getItem('11'));
+      $('#12').text(window.localStorage.getItem('12'));
+      $('#13').text(window.localStorage.getItem('13'));
+      $('#14').text(window.localStorage.getItem('14'));
+      $('#15').text(window.localStorage.getItem('15'));
+      $('#16').text(window.localStorage.getItem('16'));
+      $('#14').text(window.localStorage.getItem('17'));
+      $('#9').text(window.localStorage.getItem('9'));
+
+      // Set current time top at the top of the page
+      var currentDate = luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' });;
+         console.log(currentDate);
+         $("#currentDay").text(currentDate);
+      var currentTimeId = luxon.DateTime.local().get('hour');
+
+
+      // time-block.each()
+      $(".time-block").each(function(){
+         var timeBlockHour = this.id
+         console.log(timeBlockHour);
+         if (timeBlockHour < currentTimeId){
+           $(this).find('textarea').addClass('past');
+         }
+         else if (timeBlockHour > currentTimeId){
+         $(this).find('textarea').addClass('future');
+         }
+         else {
+         $(this).find('textarea').addClass('present');
+         }
+      });
